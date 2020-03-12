51793d853b488e98e47778ac7f0096136d78ebfa
Stephen Horne <fatste@gmail.com>
2020-03-12 12:49:27 +0100

    Remove local dependency

--- Package.swift
+++ Package.swift
@@ -13,14 +13,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(path: "../KeyboardMonitor"),
+//         .package(url: "https://github.com/ryecroft/KeyboardMonitor.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "TableController",
-            dependencies: ["KeyboardMonitor"]),
+            dependencies: []),
         .testTarget(
             name: "TableControllerTests",
             dependencies: ["TableController"]),
